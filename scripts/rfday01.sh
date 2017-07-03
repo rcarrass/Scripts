494353c3d67364ee273ef98efa74e06afc193573
remi <rcarrass42@gmail.com>
2017-07-03 02:50:41 +0200

    config

---
+++ scripts/rfday01.sh
@@ -0,0 +1,16 @@
+cd ~/work
+mkdir day01
+cd day01
+mkdir ex01 ex02 ex03 ex04 ex05 ex06 ex07 ex08 ex09
+touch\
+	ex01/print_groups.sh\
+	ex02/find_sh.sh\
+	ex03/count_files.sh\
+	ex04/MAC.sh\
+	ex05/\"\\\?\$\*\'KwaMe\'\*\$\?\\\"\$\
+	ex06/skip.sh\
+	ex07/r_dwssap.sh\
+	ex08/bon.sh\
+	ex09/add_chelou.sh
+
+echo "Day01 created."
---
+++ sh.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+cp config/.vimrc ~/.vimrc
+cp config/.zshrc ~/.zshrc
+mkdir ~/work
+cd scripts
+sh rfday00.sh
